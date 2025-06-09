ccb1dbb55088629c6ab0cfdbdc4f1b0cf5f35cc5
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-06-09 19:02:06 +0900

    feat: between関数実装
    範囲検索のため、2分探索可能なデータ構造に修正
    
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,15 +2,76 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { parse } from 'yaml'
 
-const text = fs.readFileSync(path.join(import.meta.dirname, '../dataset/holidays.yml'), 'utf-8')
-const dataset: Record<string, string> = parse(text.toString())
+// 基準日
+const EPOCH_MILLISECONDS = Date.UTC(1970, 0, 1)
 
-const code = ['// Generated from holidays.yml']
-code.push(
-  `const d: Set<number> = new Set([${Object.keys(dataset)
-    .map((d) => d.replaceAll('-', ''))
-    .join(',')}])`,
-)
-code.push('export { d as holidays }')
+const DATE_MILLISECONDS = 1000 * 60 * 60 * 24
+
+const toEpochDay = (date: string): number => {
+  const [year, month, day] = date.split('-').map(Number)
+  const targetDate = Date.UTC(year, month - 1, day)
+
+  const diffTime = targetDate - EPOCH_MILLISECONDS
+  const diffDays = Math.floor(diffTime / DATE_MILLISECONDS)
+  return diffDays
+}
+
+const text = fs.readFileSync(path.join(import.meta.dirname, '../dataset/holidays_detailed.yml'), 'utf-8')
+const dataset: { [date: string]: { date: string; name: string; name_en: string } } = parse(text.toString())
+
+// 祝日名を集約する
+const uniqueNames = new Map<string, string>()
+for (const info of Object.values(dataset)) {
+  const nameJa = info.name
+  const nameEn = info.name_en
+  if (nameJa && nameEn) {
+    uniqueNames.set(nameJa, nameEn)
+  }
+}
+// 祝日名の並び順を一定にする
+const holidayNames = Array.from(uniqueNames.entries()).sort((a, b) => {
+  return a[0].localeCompare(b[0], 'ja', { sensitivity: 'base' })
+})
+
+const nameToIndexMap = new Map<string, number>()
+holidayNames.forEach(([nameJa], index) => {
+  nameToIndexMap.set(nameJa, index)
+})
+
+const tmpHolidaysData: [number, number][] = []
+for (const info of Object.values(dataset)) {
+  const date = info.date
+  const nameJa = info.name
+  if (!date || !nameJa) {
+    console.warn(`データ欠損があります. ${JSON.stringify(info)}`)
+    continue
+  }
+  const epochDay = toEpochDay(date)
+  const index = nameToIndexMap.get(nameJa)
+  if (index === undefined) {
+    console.warn(`名前とインデックスの紐付けが不完全 ${JSON.stringify(info)}`)
+    continue
+  }
+
+  tmpHolidaysData.push([epochDay, index])
+}
+// 日付順にする
+tmpHolidaysData.sort((a, b) => a[0] - b[0])
+// 2次配列を1次配列に変換
+const holidaysData: number[] = []
+for (const [day, index] of tmpHolidaysData) {
+  holidaysData.push(day)
+  holidaysData.push(index)
+}
+
+const code = ['// Generated from holidays_detailed.yml']
+code.push(`type HolidayName = [string, string]
+
+const names: HolidayName[] = ${JSON.stringify(holidayNames)}
+
+const holidays: number[] = ${JSON.stringify(holidaysData)}
+
+export { names, holidays }
+`)
 
 fs.writeFileSync(path.join(import.meta.dirname, '../src/holidays.ts'), code.join('\n'))
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,96 @@
-import { holidays } from './holidays'
+import { holidays, names } from './holidays'
 
-const dateToNumber = (date: Date): number => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  return year * 10000 + month * 100 + day
+// 基準日
+const EPOCH_MILLISECONDS = Date.UTC(1970, 0, 1)
+
+const DATE_MILLISECONDS = 1000 * 60 * 60 * 24
+
+// 祝日データは、基準日からの日数と名称インデックスの順で格納されている
+const HOLIDAYS_LENGTH = holidays.length
+const HOLIDAYS_HIGH = (HOLIDAYS_LENGTH >> 1) - 1
+
+type Holiday = {
+  date: Date
+  nameJa: string
+  nameEn: string
 }
+
+const toEpochDay = (date: Date): number => {
+  return Math.floor((date.getTime() - EPOCH_MILLISECONDS) / DATE_MILLISECONDS)
+}
+
+const fromEpochDay = (day: number): Date => {
+  const r = new Date(1970, 0, 1)
+  r.setDate(r.getDate() + day)
+  return r
+}
+
+// 2分探索により祝日を抽出する
 
 const isHoliday = (date: Date): boolean => {
-  const index = dateToNumber(date)
-  return holidays.has(index)
+  const epochDay = toEpochDay(date)
+
+  let low = 0
+  let high = HOLIDAYS_HIGH
+  let foundIndex = -1
+
+  while (low <= high) {
+    const mid = (low + high) >> 1
+    const currentDay = holidays[mid << 1]
+    if (currentDay === epochDay) {
+      // 見つかった
+      foundIndex = mid
+      break
+    }
+    if (currentDay < epochDay) {
+      low = mid + 1
+    } else {
+      high = mid - 1
+    }
+  }
+
+  return foundIndex !== -1
 }
 
-export { isHoliday }
+const between = (start: Date, end: Date): Holiday[] => {
+  const epochStartDay = toEpochDay(start)
+  const epochEndDay = toEpochDay(end)
+
+  let low = 0
+  let high = HOLIDAYS_HIGH
+  let startIndex = 0
+
+  // 開始日以降のデータ位置抽出
+  while (low <= high) {
+    const mid = (low + high) >> 1
+    const currentDay = holidays[mid << 1]
+    if (currentDay < epochStartDay) {
+      low = mid + 1
+    } else {
+      startIndex = mid
+      high = mid - 1
+    }
+  }
+
+  const result: Holiday[] = []
+  let i = startIndex << 1
+  do {
+    const date = holidays[i]
+    const nameIndex = holidays[i + 1]
+    if (date > epochEndDay) {
+      break
+    }
+    const name = names[nameIndex]
+    result.push({
+      date: fromEpochDay(date),
+      nameJa: name[0],
+      nameEn: name[1],
+    })
+    i += 2
+  } while (i < HOLIDAYS_LENGTH)
+
+  return result
+}
+
+export { between, isHoliday }
+export type { Holiday }
