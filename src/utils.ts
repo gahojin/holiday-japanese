ff6951203fbf30f66653371b2d28d770458dd534
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-07-11 08:45:20 +0900

    fix: 日数を1つ前の祝日との差に変更することにより、データサイズ削減
    holidays.ts: 7,531 -> 5,592

--- src/utils.ts
+++ src/utils.ts
@@ -5,16 +5,15 @@ const decodeHolidays = (data: string): number[] => {
   const t = isNode ? Buffer.from(data, 'base64') : Uint8Array.from(atob(data), (c) => c.charCodeAt(0))
   const view = new DataView(t.buffer, t.byteOffset, t.byteLength)
   const len = view.byteLength
-  const n = Math.floor(len / 3)
+  const n = len >> 1
   const holidays = new Array<number>(n << 1)
 
   let offset = 0
-  let i = 0
+  let day = 0
   while (offset < len) {
-    holidays[i++] = view.getUint16(offset, true)
-    offset += 2
-    holidays[i++] = view.getUint8(offset)
-    offset += 1
+    day += view.getUint8(offset)
+    holidays[offset++] = day
+    holidays[offset] = view.getUint8(offset++)
   }
   return holidays
 }
