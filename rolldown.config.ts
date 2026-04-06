76f7785eb447f33d2a71aedcbb5581885fad86f3
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-04-06 18:04:00 +0900

    fix: esmオンリーとする
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'rolldown'
-import IsolatedDecl from 'unplugin-isolated-decl/rolldown'
+import { dts } from 'rolldown-plugin-dts'
 
 export default defineConfig([
   {
@@ -12,10 +12,7 @@ export default defineConfig([
       nativeMagicString: true,
     },
     input: 'src/index.ts',
-    output: [
-      { dir: 'dist', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true, cleanDir: true },
-      { dir: 'dist', format: 'cjs', entryFileNames: '[name].cjs', sourcemap: true, exports: 'named' },
-    ],
-    plugins: [IsolatedDecl()],
+    output: [{ dir: 'dist', format: 'es', sourcemap: true, cleanDir: true }],
+    plugins: [dts()],
   },
 ])
